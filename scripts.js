b4f8cebbee35c5db7986689dbb1cf4db9bf5a875
Nicolas <dev@houseofnd.com.ar>
2024-09-05 16:59:23 -0300

    hamburger menu created

---
+++ scripts.js
@@ -0,0 +1,12 @@
+/* Menu hamburguesa*/
+const nav = document.querySelector('#nav');
+const abrir = document.querySelector('#abrir');
+const cerrar = document.querySelector('#cerrar');
+
+abrir.addEventListener('click', () => {
+    nav.classList.add('visible');
+});
+cerrar.addEventListener('click', () => {
+    nav.classList.remove('visible');
+});
+/* Fin Menu hamburguesa*/
