35c03ff3204adb0d97df5533d8fdc013cc733ca6
Nicolas <dev@houseofnd.com.ar>
2024-09-07 18:06:38 -0300

    V2 finished and ready to PR
    - Glider.JS integrated
    -Photos added
    -Bug fixes

--- scripts.js
+++ scripts.js
@@ -10,3 +10,33 @@ cerrar.addEventListener('click', () => {
     nav.classList.remove('visible');
 });
 /* Fin Menu hamburguesa*/
+/* Glider.js inicialization */
+window.addEventListener('load', function () {
+    [].forEach.call(document.querySelectorAll('.glider'), function (ele) {
+    ele.addEventListener('glider-slide-visible', function (event) {
+    var glider
+            = Glider(this); console.log('Slide Visible %s', event.detail.slide)
+    });
+    ele.addEventListener('glider-slide-hidden', function (event) {
+    console.log('Slide Hidden %s', event.detail.slide)
+    });
+    ele.addEventListener('glider-refresh', function (event) {
+    console.log('Refresh')
+    });
+    ele.addEventListener('glider-loaded', function
+            (event) {
+    console.log('Loaded')
+    });
+    new Glider(ele, {
+    slidesToShow: 1,
+            scrollLock: true,
+            draggable: true,
+            dots: ele.parentNode.querySelector('.dots'),
+            arrows: {
+            prev: ele.parentNode.querySelector('.glider-prev'),
+                    next: ele.parentNode.querySelector('.glider-next')
+            }
+    });
+    });
+    });
+/* Fin Glider.js inicialization */
